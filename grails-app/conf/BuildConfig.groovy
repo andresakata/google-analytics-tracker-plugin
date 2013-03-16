79efa1a748ff9647aaa8a428fabe8fc537240159
Burt Beckwith <burt@burtbeckwith.com>
2013-03-16 15:52:58 -0400

    cleanup

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,31 +1,17 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = 'target'
 
 grails.project.dependency.resolution = {
-    inherits("global") {
-    }
 
-    log "warn"
+	inherits 'global'
+	log 'warn'
 
-    repositories {
-        grailsCentral()
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-    }
+	repositories {
+		grailsCentral()
+	}
 
-    dependencies {
-    }
-
-    plugins {
-        build(":tomcat:$grailsVersion",
-              ":release:2.2.0",
-              ":rest-client-builder:1.0.3") {
-            export = false
-        }
-    }
+	plugins {
+		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+			export = false
+		}
+	}
 }
